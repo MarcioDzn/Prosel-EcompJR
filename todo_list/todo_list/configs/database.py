f67c090f2b6b1d7a45ea4ec5979d8dfefde4d152
MarcioDzn <79764696+MarcioDzn@users.noreply.github.com>
2024-09-08 19:09:59 -0300

    fix: correct Settings import

--- todo_list/todo_list/configs/database.py
+++ todo_list/todo_list/configs/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from todo_list.todo_list.configs.settings import Settings
+from todo_list.configs.settings import Settings
 
 engine = create_engine(Settings().DATABASE_URL)
 
