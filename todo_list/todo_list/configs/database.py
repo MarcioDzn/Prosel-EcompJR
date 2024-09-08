a0b3f05da25c94013a0d14d9aaff288c7ad2b3cc
MarcioDzn <79764696+MarcioDzn@users.noreply.github.com>
2024-09-08 16:49:58 -0300

    chore: configure sqlalchemy settings

---
+++ todo_list/todo_list/configs/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from todo_list.todo_list.configs.settings import Settings
+
+engine = create_engine(Settings().DATABASE_URL)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
