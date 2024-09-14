c20337d4596e2c0d542262e1b6ddbb9fc9b21320
emersonrlp <emersonrodrigolimapereira@gmail.com>
2024-09-13 22:02:09 -0300

    feat: add task route

--- todo_list/todo_list/app.py
+++ todo_list/todo_list/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from todo_list.routes.users import router as usersRoute
 from todo_list.routes.auth import router as authRoute
+from todo_list.routes.tasks import router as tasksRoute
 
 app = FastAPI()
 
 app.include_router(usersRoute)
 app.include_router(authRoute)
+app.include_router(tasksRoute)
